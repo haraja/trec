3ef71d07d4757bfe9d596ab55cf88f70b43df9bd
haraja <hrajalin@hotmail.com>
2019-01-21 21:17:35 +0100

    minor code beautifications, comment updates

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
 # TReg
 Program to recognize handwritten characters
 
+## Why trec?
+To practice implementing deep neural network, to be used in learning characters.
+All algorithms are written from the scratch. Readily available libraries are
+used primarily for matrix operations. Target is to work with big dataset, so
+usage of for-loops is avoided and instead matrix-operations are preferred.
+
 ## Getting started
 Needed data of the character-sets, which is used for learning, is from MNIST.
-This data is not includedin this Git project. You need to download the sets from:
+This data is not included in this Git project. You need to download the sets from:
 http://yann.lecun.com/exdb/mnist/
+
+Notice, that files need to be extracted and placed in same folder with py-files
 
 ## Status
 This project is not finished, and not functional yet
+Only one hidden layer is initially supported
 
 ## Ideas during development
 Initially task is to implement machine learning algorithm, using
 deep neural network, to learn characters
 
 
-Ideas afterwards
-Proceed in steps:
--recognize single characters
--recognize strings
-
-Optional
+Next:
+-start to use tanh/relU instead of sigmoid function
 -split "test-set" to dev- & test-set
--- mlfunc.py
+++ mlfunc.py
@@ -1,5 +1,5 @@
 '''
-Machine learning functions 
+Machine learning functions
 '''
 
 
@@ -7,66 +7,66 @@ import numpy as np
 
 
 def sigmoid(Z):
-    """Sigmoid activation function. Works with numbers and matrices
+    '''Sigmoid activation function. Works with numbers and matrices
 
     Args:
     Z -- real value / matrix
 
     Returns:
     float number, activation with sigmoid function
-    """
+    '''
 
     return 1 / (1 + np.exp(-Z))
 
 
 def labels_to_vectors(Y_labels):
-    """Codes each label in array to character array to be used in neural network
+    '''Codes each label in array to character array to be used in neural network
     Each label will be represented by 10 character vector of 0's and 1
     for example:
         0: 1000000000
         1: 0100000000
         2: 0010000000
         ...etc
-    notice that each number is presented in one column in array 
+    notice that each number is presented in one column in array
     (not row, as could be understood from example above)
-    
+
     Args:
     Y_labels -- vector of labels
-    
+
     Returns:
     Y_vectors -- Array of all labels represented in above-mentioned format
-    """
-    
+    '''
+
     m = Y_labels.size
     Y_vectors = np.zeros((10, m))
 
     for i in range(m):
         Y_vectors[Y_labels[0, i], i] = 1
-        
+
     return Y_vectors
-    
+
 
 def vectors_to_labels(Y_vectors):
-    """Codes set of arrays, representing numbers, to labels
-    """
-    
+    '''Codes set of arrays, representing numbers, to labels
+    '''
+
     n = Y_vectors.shape[0]
     m = Y_vectors.shape[1]
-    
+
     Y_labels = np.zeros((1, m))
-    
+
     for i in range(m):
         for j in range(n):
             if Y_vectors[j, i] == 1:
                 Y_labels[0, i] = j
                 break
-        
+
     return Y_labels
-    
+
 
 
 def init_params():
-    """Initializes the parametersof neural net
+    '''Initializes the parametersof neural net
 
     Initially implemented for neural network with 1 hidden layer only
     (and one input and one output layer)
@@ -77,8 +77,8 @@ def init_params():
         b1 -- bias vector of layer 1, shape: hidden_layer x 1
         W2 -- weight matrix of layer 2, shape output_layer x hidden_layer
         b2 -- bias vector of layer 2, shape output_layer x 1
-    """
-    
+    '''
+
     # TODO: Get hardcoded values from below rather from image dimension etc.
     n_x = 784   # size of input layer - size of 1 image
     n_h = 15    # size of hidden layer
@@ -92,14 +92,14 @@ def init_params():
     b2 = np.zeros((n_y, 1))         # bias multiplier for output layer
     #b1 = np.random.rand(n_h, 1)
     #b2 = np.random.rand(n_y, 1)
-    
+
     weight_params = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
 
     return weight_params
 
 
 def forward_propagation(X, weight_params):
-    """Forward propagation of the parameters
+    '''Forward propagation of the parameters
 
     Parameters:
     X -- matrix containing images
@@ -107,13 +107,13 @@ def forward_propagation(X, weight_params):
 
     Returns:
     y_hat...
-    """
-    
+    '''
+
     W1 = weight_params["W1"]
     b1 = weight_params["b1"]
     W2 = weight_params["W2"]
     b2 = weight_params["b2"]
- 
+
     Z1 = np.dot(W1, X) + b1
     A1 = sigmoid(Z1)
     Z2 = np.dot(W2, A1) + b2
@@ -125,50 +125,48 @@ def forward_propagation(X, weight_params):
 
 
 def compute_cost(Y, A):
-    """Computes cost of for the forward propagation
+    '''Computes cost of for the forward propagation
 
     Args:
     Y -- true labels
     A -- output of the last layer's activation
-    
+
     Returns:
     cost
-    """
+    '''
 
     m = Y.shape[1]  # Number or y-units (0..9)
-    #print("compute_cost::m " + str(m))
 
     log_calc = np.multiply(np.log(A), Y) + np.multiply(np.log(1-A), (1-Y))
     cost = -1/m * np.sum(log_calc)
-    
     # print("compute_cost::cost " + str(cost))
-    
+
     return cost
 
 
 def backward_propagation(X, Y, weight_params, cache_params):
-    """Backward propagation computes delta between true values and computed weighted values
-    
+    '''Backward propagation computes delta between true values and computed weighted values
+
     Args:
     X -- input parameters (images)
     Y -- true labels
-    weight_params -- 
+    weight_params --
     cache_params -- Z, A, parameters computed during forward propagation
-    
+
     Returns:
     gradient_params --  parameters of the gradient (weight - derivative)
-    """
-    
+    '''
+
     m = Y.shape[1]  # Number or y-units (0..9)
-    
+
     # Get weights parameters from forward propagations
     W1 = weight_params["W1"]
     W2 = weight_params["W2"]
-    
+
     # Get activation parameters
     A1 = cache_params["A1"]
     A2 = cache_params["A2"]
-    
+
     # Calculate derivatives
     dZ2 = A2 - Y
     dW2 = 1/m * np.dot(dZ2, A1.T)
@@ -177,33 +175,33 @@ def backward_propagation(X, Y, weight_params, cache_params):
     #dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))    # derivative of tanh activation function
     dW1 = 1/m * np.dot(dZ1, X.T)
     db1 = 1/m * np.sum(dZ1, axis = 1, keepdims = True)
-    # NOTE: right now using sigmoid activation function is all layers. 
+    # NOTE: right now using sigmoid activation function is all layers.
     # If different activation functions would be used, then dZx would also need to change
-    
+
     gradient_params = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
-    
+
     return gradient_params
 
-    
+
 def update_params(weight_params, gradient_params):
-    """Updates weight parameters from the gradient
-    
+    '''Updates weight parameters from the gradient
+
     Args:
     X -- input array
     weight_params -- dictionary of weight and bias parameters
-    
+
     Returns:
     weight_params -- updated weight and bias parameters
-    """
-    
+    '''
+
     learning_rate = 1.2
-    
+
     # Get weights parameters
     W1 = weight_params["W1"]
     b1 = weight_params["b1"]
     W2 = weight_params["W2"]
     b2 = weight_params["b2"]
-    
+
     # Get gradient parameters
     dW1 = gradient_params["dW1"]
     db1 = gradient_params["db1"]
@@ -217,22 +215,22 @@ def update_params(weight_params, gradient_params):
     b2 = b2 - db2 * learning_rate
 
     weight_params = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
-    
-    return weight_params    
-    
+
+    return weight_params
+
 
 def check_accuracy(X, Y, weight_params):
-    """Checks the accuracy of trained network
-    """
-    
+    '''Checks the accuracy of trained network
+    '''
+
     m = X.shape[1]
-    
+
     Y_predictions = np.zeros((1, m))
-    
+
     cache_params = forward_propagation(X, weight_params)
     A2 = cache_params["A2"]
     A2_labels = vectors_to_labels(A2)
-    
+
     predict_vector = np.argmax(A2, axis = 0)
     predict_vector.shape = (1, m)
     '''
